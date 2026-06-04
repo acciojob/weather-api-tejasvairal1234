6eeccaa099dd50d9b12b739f6a1587ce9ea3b827
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-04 10:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+const txt = document.getElementById("weatherData");
+
+async function weatherData() {
+  try
+  {
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ce7bedd96d9a4c5a083de8283c5a7b60");
+    let data = await response.json();
+    return data;
+  }
+  catch(err)
+  {
+    return err;
+  }
+  
+}
+
+btn.addEventListener("click", async () => 
+	{
+		let Data = await weatherData();
+		txt.textContent = `Current weather in London: ${Data.weather[0].main}`;
+	})
