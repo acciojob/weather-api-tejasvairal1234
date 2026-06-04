cc6c1f5b8ab1c864630eb673346798951790a6b8
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-04 10:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const txt = document.getElementById("weatherData");
 async function weatherData() {
   try
   {
-    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ce7bedd96d9a4c5a083de8283c5a7b60");
+    let response = await fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ce7bedd96d9a4c5a083de8283c5a7b60"
+);
     let data = await response.json();
     return data;
   }
